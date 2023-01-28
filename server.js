9b42624eeffc94c364ccd80918369341dbc5225c
Joshua George <horheyinc8@gmail.com>
2023-01-29 00:01:43 +0100

    uploader file

--- server.js
+++ server.js
@@ -8,60 +8,60 @@ app.listen(PORT, () => {
 //---------------------------------------------------------
 //---------------------------------------------------------
 
-//-----------------SOCKET.IO SETUP-------------------------------
-let myServer = app.listen(SOCKET_PORT, () => {
-  console.log(
-    `Socket Server listening on port http://localhost:${SOCKET_PORT}`
-  );
-});
+// //-----------------SOCKET.IO SETUP-------------------------------
+// let myServer = app.listen(SOCKET_PORT, () => {
+//   console.log(
+//     `Socket Server listening on port http://localhost:${SOCKET_PORT}`
+//   );
+// });
 
-const { Server } = require("socket.io");
-const io = new Server(myServer, {
-  cors: {
-    origin: "*",
-  },
-});
+// const { Server } = require("socket.io");
+// const io = new Server(myServer, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
 
-//-------------------SOCKET EVENTS -----------------------------
+// //-------------------SOCKET EVENTS -----------------------------
 
-const MessageModel = require("./models/Message.model");
+// const MessageModel = require("./models/Message.model");
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
 
-  socket.on("join_chat", (data) => {
-    socket.join(data);
-    console.log("User Joined Room: " + data);
-  });
+//   socket.on("join_chat", (data) => {
+//     socket.join(data);
+//     console.log("User Joined Room: " + data);
+//   });
 
-  socket.on("send_message", async (data) => {
-    const {
-      content: { sender, message },
-      chatId,
-    } = data;
-    let newMessage = {
-      sender: sender,
-      message: message,
-      conversationId: chatId,
-    };
+//   socket.on("send_message", async (data) => {
+//     const {
+//       content: { sender, message },
+//       chatId,
+//     } = data;
+//     let newMessage = {
+//       sender: sender,
+//       message: message,
+//       conversationId: chatId,
+//     };
 
-    // As the conversation happens, keep saving the messages in the DB
-    let newMessageDB = await MessageModel.create(newMessage);
-    let populatedMessage = await MessageModel.findById(
-      newMessageDB._id
-    ).populate("sender");
+//     // As the conversation happens, keep saving the messages in the DB
+//     let newMessageDB = await MessageModel.create(newMessage);
+//     let populatedMessage = await MessageModel.findById(
+//       newMessageDB._id
+//     ).populate("sender");
 
-    let responseMessage = {
-      sender: {
-        name: populatedMessage.sender.name,
-      },
-      message: populatedMessage.message,
-    };
-    socket.to(data.chatId).emit("receive_message", responseMessage);
-  });
-});
+//     let responseMessage = {
+//       sender: {
+//         name: populatedMessage.sender.name,
+//       },
+//       message: populatedMessage.message,
+//     };
+//     socket.to(data.chatId).emit("receive_message", responseMessage);
+//   });
+// });
 
 //---------------------------------------------------------------
